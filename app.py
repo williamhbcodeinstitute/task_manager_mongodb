6d139a7d913c13b106d9af597e303852ca9c4aaa
williamhbcodeinstitute <william.h.barnes@outlook.com>
2020-02-27 18:33:26 +0000

    Added Nav functionality

--- app.py
+++ app.py
@@ -2,19 +2,20 @@ import os
 from flask import Flask, render_template, redirect, request, url_for, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
 mongo = PyMongo(app)
 
 @app.route('/')
+
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", 
-                           tasks=mongo.db.tasks.find())
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
 @app.route('/add_task')
